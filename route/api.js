b9555a79a45a055166fbe33ea56dcc58da4573d4
AoranZhao <aoran_zhao@yahoo.ca>
2019-01-21 08:07:20 -0500

    add auth to all api

--- route/api.js
+++ route/api.js
@@ -18,73 +18,99 @@ apiAuthRoute.get('/verify', (req, res) => {
 
 apiGradingRoute.get('/', (req, res) => {
     // api.authorize(req, res, (request, response) => {
-    response.status(200).send('succeed');
+    res.status(200).send('succeed');
     // response.status(200).send(request.headers['x-user']);
     // })
 })
 
 apiGradingRoute.get('/references', (req, res) => {
-    api.ref_getList(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.ref_getList(request, response);
+    });
 })
 
 apiGradingRoute.get('/references/:id', (req, res) => {
-    api.ref_getRef(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.ref_getRef(request, response);
+    });
 })
 
 apiGradingRoute.post('/references/:id', (req, res) => {
-    api.ref_postRef(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.ref_postRef(request, response);
+    });
 })
 
 apiGradingRoute.delete('/references/:id', (req, res) => {
-    api.ref_deleteRef(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.ref_deleteRef(request, response);
+    });
 })
 
 apiGradingRoute.get('/references/:refId/solutions', (req, res) => {
-    api.solu_getList(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.solu_getList(request, response);
+    });
 })
 
 apiGradingRoute.get('/references/:refId/solutions/:soluId', (req, res) => {
     api.authorize(req, res, (request, response) => {
         api.solu_getSolu(request, response);
-    })
+    });
 })
 
 apiGradingRoute.post('/references/:refId/solutions/:soluId', (req, res) => {
     api.authorize(req, res, (request, response) => {
         api.solu_postSolu(request, response);
-    })
+    });
 })
 
 apiGradingRoute.delete('/references/:refId/solutions/:soluId', (req, res) => {
-    api.solu_deleteSolu(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.solu_deleteSolu(request, response);
+    });
 })
 
 apiGradingRoute.get('/ocr', (req, res) => {
-    api.lineseg_getList(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.lineseg_getList(request, response);
+    });
 })
 
 apiGradingRoute.get('/ocr/:id', (req, res) => {
-    api.lineseg_getSeg(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.lineseg_getSeg(request, response);
+    });
 })
 
 apiGradingRoute.post('/ocr/:id', (req, res) => {
-    api.lineseg_postSeg(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.lineseg_postSeg(request, response);
+    });
 })
 
 apiGradingRoute.delete('/ocr/:id', (req, res) => {
-    api.lineseg_deleteSeg(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.lineseg_deleteSeg(request, response);
+    });
 })
 
 apiGradingRoute.get('/ocr/:scan_id/images', (req, res) => {
-    api.image_getImages(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.image_getImages(request, response);
+    });
 })
 
 apiGradingRoute.post('/ocr/:scan_id/images', multiParty, (req, res) => {
-    api.image_postImages(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.image_postImages(request, response);
+    });
 })
 
 apiGradingRoute.delete('/ocr/:scan_id/images/:image_id', (req, res) => {
-    api.image_deleteImages(req, res);
+    api.authorize(req, res, (request, response) => {
+        api.image_deleteImages(request, response);
+    });
 })
 
 module.exports = {
